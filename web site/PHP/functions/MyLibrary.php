9a4897d93c3f28e3427e60b3a68d0c45ddd8836a
Daniele <113792421+Daniele003@users.noreply.github.com>
2024-10-23 10:56:39 +0200

    ?

--- web site/PHP/functions/MyLibrary.php
+++
@@ -1,6 +0,0 @@
-<?php
-        function clearText($text)
-        {
-            return str_replace("'", "\'", str_replace("\"", "\\\"", str_replace("\n", "", str_replace("\r", "", $text))));
-        }
-?>
